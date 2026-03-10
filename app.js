d8910705ca7d8de82084a4c6a1d77cb66f7b4d64
aviationkjs@naver.com <aviationkjs@naver.com>
2026-03-10 19:08:48 +0900

    3월 10일 시작

--- app.js
+++ app.js
@@ -1,74 +1,198 @@
-// 사용자 이름
-let user = localStorage.getItem("user")
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-document.getElementById("user").innerText = user
+import {
+getDatabase,
+ref,
+push,
+onValue,
+update,
+remove
+} from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-// Firebase 설정
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
+ apiKey: "AIzaSyBsOQMj6GTx1GVJVqjurZGlk0c1v1MjNOk",
+  authDomain: "board-sheet-metal.firebaseapp.com",
+  databaseURL: "https://board-sheet-metal-default-rtdb.firebaseio.com",
+  projectId: "board-sheet-metal",
+  storageBucket: "board-sheet-metal.firebasestorage.app",
+  messagingSenderId: "300454183349",
+  appId: "1:300454183349:web:7a5e681d9c6c1f7b5999fc",
+  measurementId: "G-BK5QB5S01P"
+};
+
+const app=initializeApp(firebaseConfig);
+const db=getDatabase(app);
+
+const notesRef=ref(db,"notes");
+
+const board=document.getElementById("board");
+const usernameDiv=document.getElementById("username");
+
+
+// 사용자 이름 저장/불러오기
+
+let username=localStorage.getItem("username");
+
+if(!username){
+
+username=prompt("이름을 입력하세요");
+
+localStorage.setItem("username",username);
+
 }
 
-// Firebase v8 CDN 사용
-firebase.initializeApp(firebaseConfig)
+usernameDiv.innerText="👤 "+username;
 
-const db = firebase.database()
 
 // 스티커 추가
-function addNote(){
 
-let text = prompt("의견을 입력하세요")
+window.addNote=()=>{
 
-if(!text) return
+push(notesRef,{
+user:username,
+text:"아이디어",
+x:Math.random()*300,
+y:Math.random()*300,
+vote:0
+});
 
-db.ref("notes").push({
-  user:user,
-  text:text,
-  likes:0
-})
+};
+
+
+// 전체 삭제 기능
+
+window.deleteAll = ()=>{
+
+const pw = prompt("관리자 비밀번호");
+
+if(pw === "1234"){
+
+if(confirm("모든 스티커를 삭제하시겠습니까?")){
+
+remove(notesRef);
 
 }
 
-// 실시간 표시
-db.ref("notes").on("value", function(snapshot){
+}else{
 
-let board = document.getElementById("board")
+alert("비밀번호가 틀렸습니다");
 
-board.innerHTML=""
+}
 
-snapshot.forEach(function(data){
+};
 
-let n=data.val()
-let key=data.key
 
-let div=document.createElement("div")
 
-div.className="note"
+// 실시간 로딩
 
-div.innerHTML=
-"<b>"+n.user+"</b><br>"+
-n.text+
-"<br><button onclick='likeNote(\""+key+"\")'>👍 "+n.likes+"</button>"
+onValue(notesRef,(snap)=>{
 
-board.appendChild(div)
+board.innerHTML="";
 
-})
+const data=snap.val();
 
-})
+for(const key in data){
 
-// 좋아요
-function likeNote(key){
+const n=data[key];
 
-let ref=db.ref("notes/"+key)
+const note=document.createElement("div");
+note.className="note";
 
-ref.once("value",function(snap){
+note.style.left=n.x+"px";
+note.style.top=n.y+"px";
 
-let likes=snap.val().likes+1
+note.innerHTML=`
 
-ref.update({likes:likes})
+<b>${n.user}</b>
 
-})
+<textarea>${n.text}</textarea>
+
+<div class="vote">👍 ${n.vote}</div>
+
+<button class="delete">삭제</button>
+
+`;
+
+board.appendChild(note);
+
+
+// 텍스트 수정
+
+const textarea=note.querySelector("textarea");
+
+textarea.onchange=()=>{
+
+update(ref(db,"notes/"+key),{
+text:textarea.value
+});
+
+};
+
+
+// 투표
+
+note.querySelector(".vote").onclick=()=>{
+
+update(ref(db,"notes/"+key),{
+vote:n.vote+1
+});
+
+};
+
+
+// 삭제 (관리자 비밀번호)
+
+note.querySelector(".delete").onclick=()=>{
+
+const pw=prompt("관리자 비밀번호");
+
+if(pw==="1234"){
+remove(ref(db,"notes/"+key));
+}
+
+};
+
+
+// 드래그 이동
+
+drag(note,key);
+
+}
+
+});
+
+
+// 드래그 기능
+
+function drag(el,key){
+
+let offsetX,offsetY;
+
+el.onmousedown=(e)=>{
+
+offsetX=e.offsetX;
+offsetY=e.offsetY;
+
+document.onmousemove=(e)=>{
+
+el.style.left=(e.pageX-offsetX)+"px";
+el.style.top=(e.pageY-offsetY)+"px";
+
+};
+
+document.onmouseup=()=>{
+
+update(ref(db,"notes/"+key),{
+x:parseInt(el.style.left),
+y:parseInt(el.style.top)
+});
+
+document.onmousemove=null;
+
+};
+
+};
 
 }
